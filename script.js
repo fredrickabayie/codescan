9d4935a5e6ac554b680e9c04a0671411d149b987
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-10-01 04:04:30 +0000

    actual notification

--- script.js
+++ script.js
@@ -13,7 +13,20 @@ $(function () {
             function (result) {
                 $("#code").val(result.text);
 //                    "Format: " + result.format + "\n" +
-                navigator.notification.alert(result.text, alertCallback, ['Product Scanned'], ['Done']);
+                cordova.plugins.notification.local.schedule({
+    id: 1,
+    title: "Production Jour fixe",
+    text: "Duration 1h",
+    firstAt: monday_9_am,
+    every: "week",
+    sound: "file://sounds/reminder.mp3",
+    icon: "http://icons.com/?cal_id=1",
+    data: { meetingId:"123#fg8" }
+});
+
+cordova.plugins.notification.local.on("click", function (notification) {
+    joinMeeting(notification.data.meetingId);
+});
             },
             function (error) {
                 alert("Scanning failed: " + error);

295f26c12ad149f3daa10c7321e2aaf2a094365e
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-10-01 03:43:27 +0000

    Trying the notifications plugin

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert, cordova*/
 
+function alertCallback() {
+    "use strict";
+
+}
+
 $(function () {
     "use strict";
     $("#scan_btn").click(function () {
         cordova.plugins.barcodeScanner.scan(
             function (result) {
                 $("#code").val(result.text);
-//                alert("We got a barcode\n" +
-//                    "Result: " + result.text + "\n" +
 //                    "Format: " + result.format + "\n" +
-//                    "Cancelled: " + result.cancelled);
+                navigator.notification.alert(result.text, alertCallback, ['Product Scanned'], ['Done']);
             },
             function (error) {
                 alert("Scanning failed: " + error);
